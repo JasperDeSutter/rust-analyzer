dbdbace8850909b80b04749bc2fe5cfe2e3df138
JasperDeSutter <jasper.desutter@gmail.com>
2019-08-30 12:19:03 +0200

    add monaco provider registration for hover

--- crates/wasm_demo/www/index.js
+++ crates/wasm_demo/www/index.js
@@ -1,15 +1,37 @@
-import "wasm_demo";
-import * as monaco from "monaco-editor";
+// import 'monaco-editor/esm/vs/editor/browser/controller/coreCommands.js';
+// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+// import 'monaco-editor/esm/vs/basic-languages/rust/rust.contribution';
 
-console.log('loading wasm done')
+import * as monaco from 'monaco-editor';
+import example_code from "./example-code"
 
-const code = `
-fn main() {
-    println!("Hello, world!");
-}
-`
+import "./index.css";
+
+self.MonacoEnvironment = {
+    getWorkerUrl: () => "./editor.worker.bundle.js",
+};
 
 monaco.editor.create(document.body, {
-    value: code,
+    theme: 'vs-dark',
+    value: example_code,
     language: "rust"
 });
+
+async function setupMode() {
+    console.warn('setupMode')
+    const { WorldState } = await import("wasm_demo")
+
+    const models = monaco.editor.getModels()
+    const state = new WorldState(models[0].getValue())
+
+    monaco.languages.registerCompletionItemProvider("rust", {
+        triggerCharacters: ["."],
+        provideCompletionItems: (_, position) => state.on_dot_typed(position.lineNumber, position.column),
+    });
+
+    monaco.languages.registerHoverProvider("rust", {
+        provideHover: (_, position) => state.hover(position.lineNumber, position.column)
+    })
+}
+
+setupMode()
